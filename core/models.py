587f067c02e67a1311f4f594eaac18e052167b1b
Guilherme Louro <guilherme-louro@hotmail.com>
2015-08-13 10:41:28 -0300

    create model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
+# coding: utf-8
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(_(u'Título'), max_length=200)
+    from_day = models.DateTimeField(_('De'))
+    to_day = models.DateTimeField(_('To'))
+    local = models.CharField(_('Local'), max_length=200)
+    address = models.CharField(_('Endereço'), max_length=200)
+    price = models.CharField(_(u'Preço'), max_length=20)
