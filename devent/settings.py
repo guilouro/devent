b7eddb812937a9625d0550a43a4f71b5b559ca1f
Guilherme Louro <guilherme-louro@hotmail.com>
2015-08-20 17:25:42 -0300

    configure staticfiles

--- devent/settings.py
+++ devent/settings.py
@@ -85,11 +85,11 @@ TEMPLATE_DIRS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-# STATICFILES_DIRS = (
-#     BASE_DIR.parent.child('static'),
-# )
+STATICFILES_DIRS = (
+    BASE_DIR.child('static'),
+)
 
-STATIC_ROOT = BASE_DIR.parent.child('staticfiles')
+STATIC_ROOT = BASE_DIR.child('staticfiles')
 STATIC_URL = '/static/'
 
 
