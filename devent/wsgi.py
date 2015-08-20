23b1e99ece8555fc3e4ec588d88f0125a93d527d
Guilherme Louro <guilherme-louro@hotmail.com>
2015-08-20 17:32:25 -0300

    configure djstatic

--- devent/wsgi.py
+++ devent/wsgi.py
@@ -11,4 +11,6 @@ import os
 os.environ["DJANGO_SETTINGS_MODULE"] = "devent.settings"
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
