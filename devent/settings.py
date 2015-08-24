947cd0e8f4538ca1375ef9f50950ce2f686938fe
Guilherme Louro <guilherme-louro@hotmail.com>
2015-08-24 13:47:40 -0300

    set django database in broker url

--- devent/settings.py
+++ devent/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 
     # Apps #
+    'kombu.transport.django',
     'djcelery',
     'core',
 )
@@ -70,7 +71,7 @@ DATABASES = {
 }
 
 
-BROKER_URL = 'redis://localhost:6379/0'
+BROKER_URL = 'django://'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_BACKEND='djcelery.backends.database:DatabaseBackend'
